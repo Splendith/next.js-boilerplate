6eb44503528f5276bd52229c084022e9b2ee7930
Kunuch Chutmongkolporn <kunuch@gmail.com>
2018-04-05 23:06:29 +0700

    Add sample test

--- __tests__/pages/index.test.js
+++ __tests__/pages/index.test.js
@@ -3,21 +3,30 @@
 import { shallow } from 'enzyme';
 import React from 'react';
 import renderer from 'react-test-renderer';
+import ShallowRenderer from 'react-test-renderer/shallow';
 
-import { Index } from '~/pages/index';
+import App, { Index } from '~/pages/index';
 
 describe('With Enzyme', () => {
-  it('App shows "Hello world!"', () => {
+  it('should shows "Hello! Hover me~"', () => {
     const app = shallow(<Index />);
 
-    expect(app.find('Header').children().text()).toEqual('Hello! Hover me~');
+    expect(app
+      .find('Header')
+      .children()
+      .text()).toEqual('Hello! Hover me~');
   });
 });
 
-// describe('With Snapshot Testing', () => {
-//   it('App shows "Hello world!"', () => {
-//     const component = renderer.create(<App />);
-//     const tree = component.toJSON();
-//     expect(tree).toMatchSnapshot();
-//   });
-// });
+describe('With Snapshot Testing', () => {
+  it('shallow snapshot', () => {
+    const shallowRenderer = new ShallowRenderer();
+    const snapshot = shallowRenderer.render(<Index />);
+    expect(snapshot).toMatchSnapshot();
+  });
+  it('full snapshot', () => {
+    const component = renderer.create(<App />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
