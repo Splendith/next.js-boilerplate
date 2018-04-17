ade57e0fbe9236e8a8035db8dea71b2be4b9079d
Kunuch Chutmongkolporn <kunuch@gmail.com>
2018-04-17 21:22:34 +0700

    Add webpack-bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 const commonsChunkConfig = require('@zeit/next-css/commons-chunk-config');
-
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const withCSS = require('@zeit/next-css');
 const withSass = require('@zeit/next-sass');
 const withImages = require('./.next_config/load-images');
 const withFonts = require('./.next_config/load-fonts');
+
+const { ANALYZE } = process.env;
 
 module.exports = withFonts(withImages(withSass(withCSS({
   webpack: (configParam, { isServer }) => {
@@ -17,6 +19,14 @@ module.exports = withFonts(withImages(withSass(withCSS({
     config.node = {
       fs: 'empty',
     };
+
+    if (ANALYZE) {
+      config.plugins.push(new BundleAnalyzerPlugin({
+        analyzerMode: 'server',
+        analyzerPort: isServer ? 8888 : 8889,
+        openAnalyzer: true,
+      }));
+    }
 
     return config;
   },
--- pages/testurl.js
+++ pages/testurl.js
@@ -1,12 +1,15 @@
 import Head from 'next/head';
 import PropTypes from 'prop-types';
+import dynamic from 'next/dynamic';
 
 import withRedux from 'next-redux-wrapper';
 import initStore from '~/store/';
 import { Link } from '~/routes';
 
 import Layout from '~/components/layout/Layout';
-import CounterPage from '~/components/CounterPage';
+
+// Dynamic import
+const CounterPage = dynamic(import('../components/CounterPage'));
 
 const TestUrl = props => (
   <Layout>
@@ -15,7 +18,9 @@ const TestUrl = props => (
         <title>Test URL</title>
       </Head>
       <h2>URL Result</h2>
-      <div>ID: {props.url.query.id}, Query ID: {props.url.query.queryId}</div>
+      <div>
+        ID: {props.url.query.id}, Query ID: {props.url.query.queryId}
+      </div>
       <br />
       <Link route="/">
         <a>Back to index</a>
