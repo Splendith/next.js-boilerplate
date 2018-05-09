f89e99ae13f82a86654fa71cc2923b307d25f0ac
Kunuch Chutmongkolporn <kunuch@gmail.com>
2018-05-09 19:12:43 +0700

    Update redux wrapper

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,43 @@
 import App, { Container } from 'next/app';
-import React from 'react';
+import { Provider } from 'react-redux';
+import PropTypes from 'prop-types';
 import withRedux from 'next-redux-wrapper';
 import initStore from '~/store/';
 
 import Layout from '~/components/layout/Layout';
 
 class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    return {
+      pageProps: {
+        // Call page-level getInitialProps
+        ...(Component.getInitialProps ? await Component.getInitialProps(ctx) : {}),
+      },
+    };
+  }
   render() {
-    const { Component, pageProps } = this.props;
+    const { Component, pageProps, store } = this.props;
     return (
       <Container>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <Provider store={store}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </Provider>
       </Container>
     );
   }
 }
+
+MyApp.propTypes = {
+  Component: PropTypes.func.isRequired,
+  pageProps: PropTypes.shape(),
+  store: PropTypes.shape(),
+};
+
+MyApp.defaultProps = {
+  pageProps: undefined,
+  store: undefined,
+};
 
 export default withRedux(initStore)(MyApp);
