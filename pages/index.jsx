80c108324373223e423235e8896f87845386a7be
Kunuch Chutmongkolporn <kunuch@gmail.com>
2018-09-12 03:55:29 +0700

    Comment next/dynamic

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
-import dynamic from 'next/dynamic';
+// import dynamic from 'next/dynamic';
 import styled from 'styled-components';
 import { Link } from '~/routes';
 
--- pages/testurl.jsx
+++ pages/testurl.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
-import dynamic from 'next/dynamic';
+// import dynamic from 'next/dynamic';
 import { withRouter } from 'next/router';
 import PropTypes from 'prop-types';
 import { Link } from '~/routes';
