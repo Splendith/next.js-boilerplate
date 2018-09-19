2530d127f3447f33d27a3676eca4ec3cf4fce4a2
Kunuch Chutmongkolporn <kunuch@gmail.com>
2018-09-20 06:42:48 +0700

    Change port to 3000

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const next = require('next');
 const routes = require('../routes');
 
-const port = 3001;
+const port = 3000;
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
