e5782b81cf1b6606ffd979d6e8de8b4af3966c23
Kunuch Chutmongkolporn <kunuch@gmail.com>
2018-11-15 23:04:02 +0700

    Temporarily fix #6

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,9 +3,17 @@ import App, { Container } from 'next/app';
 import { Provider } from 'react-redux';
 import PropTypes from 'prop-types';
 import withRedux from 'next-redux-wrapper';
+import Router from 'next/router';
 import initStore from '@/store/';
-
 import Layout from '@/components/layout/Layout';
+
+Router.events.on('routeChangeComplete', () => {
+  if (process.env.NODE_ENV !== 'production') {
+    const els = document.querySelectorAll('link[href*="/_next/static/css/styles.chunk.css"]');
+    const timestamp = new Date().valueOf();
+    els[0].href = `/_next/static/css/styles.chunk.css?v=${timestamp}`;
+  }
+});
 
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
