74f26f53ae1a0f8a371b18b5590517d806cbfc77
Splendith <kunuch@gmail.com>
2019-10-20 20:11:57 +0700

    Use hook instead of HOC

--- src/pages/testurl.jsx
+++ src/pages/testurl.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 import dynamic from 'next/dynamic';
-import { withRouter } from 'next/router';
-import PropTypes from 'prop-types';
+import { useRouter } from 'next/router';
 import { Link } from '~/routes';
 
 // Styled component element
@@ -11,24 +10,23 @@ import Title from '~src/components/elements/Title';
 // Dynamic import
 const CounterPage = dynamic(import('~src/components/CounterPage'));
 
-const TestUrl = ({ router }) => (
-  <CounterPage>
-    <Head>
-      <title>Test URL</title>
-    </Head>
-    <Title>URL Result</Title>
-    <div>
-      ID: {router.query.id}, Query ID: {router.query.queryId}
-    </div>
-    <br />
-    <Link route="/">
-      <a>Back to index</a>
-    </Link>
-  </CounterPage>
-);
-
-TestUrl.propTypes = {
-  router: PropTypes.shape().isRequired,
+const TestUrl = () => {
+  const router = useRouter();
+  return (
+    <CounterPage>
+      <Head>
+        <title>Test URL</title>
+      </Head>
+      <Title>URL Result</Title>
+      <div>
+        ID: {router.query.id}, Query ID: {router.query.queryId}
+      </div>
+      <br />
+      <Link route="/">
+        <a>Back to index</a>
+      </Link>
+    </CounterPage>
+  );
 };
 
-export default withRouter(TestUrl);
+export default TestUrl;
